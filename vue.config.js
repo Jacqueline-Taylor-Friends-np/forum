225de509836bbae632ec105e4b67577c045d06b0
rmasonh22 <106461878+rmasonh22@users.noreply.github.com>
2022-08-18 17:50:10 -0400

    update site ~ customizations phase 1

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      title: 'Jacqueline Taylor & Friends',
+    },
+  }
