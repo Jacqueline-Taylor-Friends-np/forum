65d73eac18d49528d290758ff922756d83fd07e7
rmasonh22 <106461878+rmasonh22@users.noreply.github.com>
2022-08-18 23:13:04 -0400

    change helloworld componenet and add title from mock data to home page

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -2,6 +2,9 @@
   <div><h2>Jacqueline's Forum</h2></div> <!-- TODO: google forum -->
   <div v-for="thread in threads" :key="thread.id">
       <h2>{{thread.title}}</h2>
+       <div v-for="postId in thread.posts" :key="postId">
+         <p>{{posts.find(p => p.id === postId).text}}</p>
+       </div>
   </div>
 
 
@@ -13,7 +16,9 @@ import sourceData from '@/data.json'
 export default {
   data() {
     return {
-      threads: sourceData.threads
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
     }
   }
 }
