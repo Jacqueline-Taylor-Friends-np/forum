410054aeca57d56273701327d9b40d2a97fb3935
rmasonh22 <106461878+rmasonh22@users.noreply.github.com>
2022-08-18 23:29:01 -0400

    add posts to title

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -3,6 +3,7 @@
   <div v-for="thread in threads" :key="thread.id">
       <h2>{{thread.title}}</h2>
        <div v-for="postId in thread.posts" :key="postId">
+         <p>{{users.find(u => u.id === posts.find(p => p.id === postId).userId).name}}</p>
          <p>{{posts.find(p => p.id === postId).text}}</p>
        </div>
   </div>
